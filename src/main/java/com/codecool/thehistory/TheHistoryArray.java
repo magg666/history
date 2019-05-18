dea94e51eabcfdd29bd71d252e9217e222d86f0c
magg666 <mw23127@gmail.com>
2019-05-18 17:55:10 +0200

    working ArrayHistory

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -11,32 +11,76 @@ public class TheHistoryArray implements TheHistory {
 
     @Override
     public void add(String text) {
+        String textWithoutUnnecessarySigns =
+                text
+                        .replace("\n", " ")
+                        .replace("\t", " ")
+                        .trim()
+                        .replaceAll(" +", " ");
+
+        String[] parts;
+        parts = textWithoutUnnecessarySigns.split("\\s");
+
+        String[] combined = new String[wordsArray.length + parts.length];
+        System.arraycopy(wordsArray, 0, combined, 0, wordsArray.length);
+        System.arraycopy(parts, 0, combined, wordsArray.length, parts.length);
+        wordsArray = combined;
+
         //TODO: check the TheHistory interface for more information
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
+        String wordArrayAsString =
+                String
+                        .join(" ", wordsArray)
+                        .replaceAll(wordToBeRemoved, "")
+                        .trim()
+                        .replaceAll(" +", " ");
+
+        wordsArray = wordArrayAsString.split(" ");
+
         //TODO: check the TheHistory interface for more information
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArray.length;
     }
 
     @Override
     public void clear() {
+        wordsArray = new String[0];
         //TODO: check the TheHistory interface for more information
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
+        String regex = "\\b" + from + "\\b";
+        String wordArrayAsString =
+                String
+                        .join(" ", wordsArray)
+                        .replaceAll(regex, to);
+
+        wordsArray = wordArrayAsString.split(" ");
+
         //TODO: check the TheHistory interface for more information
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
+        String fromWordsString = String.join(" ", fromWords);
+        String toWordsString = String.join(" ", toWords);
+
+        String regex = "\\b" + fromWordsString + "\\b";
+        String wordArrayAsString =
+                String
+                        .join(" ", wordsArray)
+                        .replaceAll(regex, toWordsString);
+
+        wordsArray = wordArrayAsString.split(" ");
+
         //TODO: check the TheHistory interface for more information
     }
 
